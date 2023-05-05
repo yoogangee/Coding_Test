fabfb9130eea4925622359b7e16718107728123c
Yoojin Kyung <106925629+yoogangee@users.noreply.github.com>
2023-05-05 17:54:31 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,3 +1,5 @@
 def solution(absolutes, signs):
     list_len = len(absolutes)
-    return sum([absolutes[i] for i in range(list_len) if signs[i]]) - sum([absolutes[j] for j in range(list_len) if not signs[j]])
+    a = sum([absolutes[i] for i in range(list_len) if signs[i]])
+    b = sum([absolutes[j] for j in range(list_len) if not signs[j]])
+    return  a-b 
