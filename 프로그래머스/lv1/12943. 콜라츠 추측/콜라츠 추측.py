425954f49c9b4efd4ee48d444dce9c034956966a
Yoojin Kyung <106925629+yoogangee@users.noreply.github.com>
2023-05-05 17:13:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,15 +1,11 @@
 def solution(num):
     if num == 1:
         return 0
-    count = 0
-    while count <= 500 and num != 1:
+    for i in range(500):
         if num%2 == 0:
             num /= 2
-            count += 1
-        elif num%2 == 1:
+        elif num != 1 and num%2 == 1:
             num = num*3+1
-            count += 1
-    if count > 500:
-        return -1
-    return count
-        
+        else:
+            return i
+    return -1
