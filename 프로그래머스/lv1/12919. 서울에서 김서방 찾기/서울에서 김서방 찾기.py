dcdf2232030ce14fadb6dff7ed9ab8c101540b26
Yoojin Kyung <106925629+yoogangee@users.noreply.github.com>
2023-05-05 17:17:42 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,4 +1,2 @@
 def solution(seoul):
-    for i in range(len(seoul)):
-        if seoul[i] == "Kim":
-            return "김서방은 " + str(i) + "에 있다"
+    return "김서방은 {}에 있다".format(seoul.index("Kim"))
