470e345b4ab58c6cc2c5af335241e25c37388aeb
Yoojin Kyung <106925629+yoogangee@users.noreply.github.com>
2023-05-08 15:28:25 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,8 +1,5 @@
 def solution(n, m):
-    answer = []
     a, b = max(n,m), min(n,m)
     while b>0:
         a, b = b, a%b
-    answer.append(a)
-    answer.append(n*m/a)
-    return answer
+    return [a, n*m/a]
