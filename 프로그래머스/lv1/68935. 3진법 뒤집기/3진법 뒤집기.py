1b1e8b2a5759c3d3b7ef4fd482bcc86fe411a3b4
Yoojin Kyung <106925629+yoogangee@users.noreply.github.com>
2023-05-09 13:41:16 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,11 +1,22 @@
 def solution(n):
-    answer = 0
     temp = ''
+    answer = 0
     while n>0:
         temp = str(n%3) + temp
         n //= 3
+        
     ex = 1
     for i in temp:
         answer += ex*int(i)
         ex *= 3
     return answer
+
+# int('진법 문자열', 해당진법숫자) -> 10진수로 바꿔준다!
+'''
+def solution(n):
+    temp = ''
+    while > 0:
+        temp += str(n%3)
+        n //= 3
+    return int(temp,3)
+'''
