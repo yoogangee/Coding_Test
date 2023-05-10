d9200117346cebb68616a2d35aee65f50056a533
Yoojin Kyung <106925629+yoogangee@users.noreply.github.com>
2023-05-10 16:24:02 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,8 +1,3 @@
 def solution(n):
-    answer = ''
-    for i in range(n):
-        if i%2 == 0:
-            answer += "수"
-        else:
-            answer += "박"
-    return answer
+    answer = "수박"*n
+    return answer[:n]
