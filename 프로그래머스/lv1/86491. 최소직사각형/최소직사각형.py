481565c332ce7a4f05226821a0a16bbe7a6322f1
Yoojin Kyung <106925629+yoogangee@users.noreply.github.com>
2023-05-10 16:07:36 +0900

    [level 1] Title: 최소직사각형, Time: 13.03 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,7 @@
+def solution(sizes):
+    for i in range(len(sizes)):
+        sizes[i][0], sizes[i][1] = max(sizes[i][0], sizes[i][1]), min(sizes[i][0], sizes[i][1])
+    max_width = max(sizes[n][0] for n in range(len(sizes)))
+    max_height = max(sizes[m][1] for m in range(len(sizes)))
+        
+    return max_width * max_height
