dfd274e59e921070498a0f0581aa18a7ce2a1cac
Yoojin Kyung <106925629+yoogangee@users.noreply.github.com>
2023-08-30 13:02:59 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,9 +1,5 @@
 def solution(food):
     answer = ''
     for i in range(1,len(food)):
-        count = food[i]//2
-        answer += str(i)*count
-    reverse = answer[::-1]
-    answer += "0"
-    answer += reverse
-    return answer
+        answer += str(i)*(food[i]//2)
+    return answer + "0" + answer[::-1]
